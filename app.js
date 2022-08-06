998e8c99c3a3200d6937fe0e6b9aab75fe50eab1
reulezzz <93688158+reulezzz@users.noreply.github.com>
2022-08-06 15:25:10 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,34 @@
+const maxImg = document.querySelector('.right-panel img');
+const select = document.querySelector('select');
+const allLang = ['en','ru'];
+
+select.addEventListener('change', changeURLLanguage);
+
+
+function changeURLLanguage() {
+    let lang = select.value;
+    location.href = window.location.pathname + '#' + lang;
+    location.reload();
+}
+
+function changeLanguage() {
+    let hash = window.location.hash;
+    hash = hash.substr(1);
+    console.log(hash);
+    if (!allLang.includes(hash)) {
+        location.href = window.location.pathname + '#ru';
+        location.reload();
+    }
+    select.value = hash;
+    document.querySelector('.lng-unit').innerHTML = langArr['unit'][hash];
+    document.querySelector('.lng-chip').innerHTML = langArr['chip'][hash];
+    for (let key in langArr) {
+        let elem = document.querySelector('.lng-' + key);
+        if (elem) {
+            elem.innerHTML = langArr[key][hash];
+        }
+
+    }
+}
+
+changeLanguage();
---
+++ lang.js
@@ -0,0 +1,203 @@
+const langArr = {
+    "unit" :  {
+        "ru": "Возможности",
+        "en": "Possibilities",
+
+    },
+    "chip": {
+        "ru": "Преимущества",
+        "en": "Advantages",
+    },
+    "memory": {
+        "ru": "Контакты",
+        "en": "Contacts",
+    },
+    "memory-type": {
+        "ru": "Работа с комфортом!",
+        "en": "Work with comfort!",
+
+    },
+    "cool": {
+        "ru": "Наши услуги помогут вам!",
+        "en": "Our services will help you!",
+
+    },
+    "cool-type": {
+        "ru": "Большой выбор всего, что может вам пригодиться.",
+        "en": "Large selection of everything that may be useful for you.",
+
+    },
+    "cool-type-type": {
+        "ru": "Готовый к работе офис DanCon Coworking и управляемые офисы полностью меблированы, профессионально оборудованы, и управляются нашей командой. Экономьте деньги и время, чтобы сосредоточиться на своей работе и развивать свой бизнес. Мы позаботимся обо всем, что вам нужно для продуктивной работы.",
+        "en": "Redy to use office DanCon Coworking and managed offices are fully furnished, professionally equipped, and managed by our team. Save money and time to focus on your work and grow your business. We'll take care of everything you need to work productively.",
+
+    },
+    "more": {
+        "ru": "Наши преимущества",
+        "en": "Our advantages",
+        
+    },
+    "more-type": {
+        "ru": "Расположение",
+        "en": "Location",
+        
+    },
+    "more-type-text": {
+        "ru": "Центральный район. Удобная транспортная развязка и всё самое необходимое всегда рядом.",
+        "en": "Central District. Convenient transport interchange and everything you need is always nearby.",
+        
+    },
+    "more-type-type": {
+        "ru": "Цена",
+        "en": "Price",
+        
+    },
+    "more-type-text-a": {
+        "ru": "Предлагаем самые приятные и доступные условия аренды рабочих мест, а также лучший сервис во всем городе.",
+        "en": "We offer the most pleasant and affordable conditions for renting workplaces, as well as the best service in the whole city.",
+        
+    }, 
+    "more-type-type-type": {
+        "ru": "Атмосфера",
+        "en": "Atmosphere",
+        
+    },
+    "more-type-text-b": {
+        "ru": "Приятная, уютная и дружелюбная атмосфера. У нас вы найдете все условия для комфортной работы.",
+        "en": "Light, bright and friendly atmosphere. Here you will find all the conditions for a comfortable work.",
+        
+    },
+    "mail": {
+        "ru": "Оставьте заявку и мы с вами свяжемся!",
+        "en": "Leave a request and we will contact you!",
+        
+    },
+    "mail-a": {
+        "ru": "Имя",
+        "en": "Name",
+        
+    },
+    "mail-type": {
+        "ru": "Ваша почта",
+        "en": "E-mail",
+        
+    },
+    "mail-type-type": {
+        "ru": "Сообщение",
+        "en": "Message",
+        
+    },
+    "button": {
+        "ru": "Отправить",
+        "en": "Send",
+        
+    },
+    "faq-main": {
+        "ru": "Цены",
+        "en": "Price",
+        
+    },
+    "faq-a": {
+        "ru": "Рабочее место - 3000 грн. (мес.)",
+        "en": "Open desk- 3000 UAH",
+        
+    },
+    "faq-text": {
+        "ru": "Бронирование не требуется, приходите в любое время и используйте любой свободный стол.",
+        "en": "No reservation needed, Come anytime and use any open desk",
+        
+    },  
+    "faq-b": {
+        "ru": "Фиксированное рабочее место - 3600 грн. (мес.)",
+        "en": "Fixed desk- 3600 UAH",
+    },
+    "faq-text-a": {
+        "ru": "Держите выделенное место всегда доступным только для вас.",
+        "en": "Keep a dedicated desk always available just for you",
+        
+    },
+    "faq-c": {
+        "ru": "Личный кабинет до 6 человек - 21000 грн. (мес.)",
+        "en": "Private Office for up to 6 people- 21000 UAH",
+    },
+    "faq-text-b": {
+        "ru": "Арендуйте отдельную комнату только для своей команды.",
+        "en": "Rent a private room for just your team, Includes access to shared facilities like kitchen, bathroom,printer, ete",
+        
+    },
+    "faq-text-c": {
+        "ru": "*Арендуя рабочее место вы получаете доступ к общим объектам, таким как кухня, душевая комната, и т.д.",
+        "en": "*By renting a workplace, you get access to common facilities such as a kitchen, shower room, etc.",
+        
+    },
+    "what-1": {
+        "ru": "Что мы предлагаем:",
+        "en": "What we offer:",
+    },
+    "what-2": {
+        "ru": "Фиксированные рабочие места",
+        "en": "Fixed workspaces",
+    }, 
+    "what-3": {
+        "ru": "Удобные кресла",
+        "en": "Comfortable chairs",
+    }, 
+    "what-4": {
+        "ru": "Приятная, уютная и дружелюбная атмосфера.",
+        "en": "Light, bright and friendly atmosphere.",
+    }, 
+    "what-5": {
+        "ru": "Высокая скорость wi-fi",
+        "en": "High speed wi-fi",
+    }, 
+    "what-6": {
+        "ru": "Безлимитные кофе и чай",
+        "en": "Unlimited Coffee and Tea",
+    }, 
+    "what-7": {
+        "ru": "Удобное расположение не далеко от центра города",
+        "en": "Convenient location near the City Center",
+    }, 
+    "what-8": {
+        "ru": "Доступ к принтеру / сканеру",
+        "en": "Printer/Scanner access",
+    },
+    "about-1": {
+        "ru": "Адрес:",
+        "en": "Address:",
+    },
+    "about-2": {
+        "ru": "Связь с нами:",
+        "en": "Contact us:",
+    },
+    "about-3": {
+        "ru": "Часы работы:",
+        "en": "Opening hours:",
+    },
+    "about-text-a": {
+        "ru": "ул. Романа Шухевича, 68",
+        "en": "st. Roman Shukhevych, 68",
+    },
+    "about-text-b": {
+        "ru": "+380636740822 - info@dancon.work",
+        "en": "+380636740822 - info@dancon.work",
+    },
+    "about-text-c": {
+        "ru": "С 9:00 до 21:00 с понедельника по субботу (выходной - воскресенье).",
+        "en": "From 9:00-21:00 Monday-Saturday (closed Sundays)",
+    },
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
